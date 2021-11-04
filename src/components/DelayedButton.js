5162177adf24f92d70c493677e5298a9be31b66d
OLGA <olichka23.85@gmail.com>
2021-11-04 19:18:43 -0400

    commit tree 9156f26f89bfe228d22dee9c138c1866f335e419

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,18 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+    onClick(event) {
+        event.persist();
+        
+        setTimeout(() => {
+            this.props.onDelayedClick(event)
+        }, this.props.delay)
+        
+        
+    }
+
+    render() {
+        return (<button onClick={this.onClick.bind(this)}>Two</button>)
+    }
+}
